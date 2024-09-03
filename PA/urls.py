e284d31911866821de1e4754c4e63c08451bd3c5
hxllhx <lhx_gjjy19@163.com>
2024-09-03 22:48:36 +0100

    11

--- PA/urls.py
+++ PA/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.PA1),
     path('source/', views.sourcesA),
-    path(r'PA/chat/$', views.ChatConsumer.as_asgi())
+    path(r'PA/chat/', views.ChatConsumer.as_asgi())
 ]
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&z%du)@v9z6m7rf&qmbd=l6(pysablvc85l5pmz(1x2+9!auw_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
